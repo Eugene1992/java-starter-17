9584093aaff474747e5f080d8553dde32aad7910
Igor Malyshko <malishkoio@gmail.com>
2017-01-12 22:45:32 +0200

    возврат к началу делаем вид что ничего не было

--- igor/src/main/java/Ihor.java
+++ igor/src/main/java/Ihor.java
@@ -17,37 +17,6 @@ import java.util.Scanner;
 public class Ihor {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //task 1-5
-        byte tempByte = 5 + 7;
-        short tempShort = 5 - 10;
-        int tempInt = 13 / 7;
-        long tempLong = 2 * 3;
-        System.out.println(tempByte + " " + tempShort + " " + tempInt + " " + tempLong);
-        //task 6
-        byte task6_0, task6_1 = 125, task6_2 = 10;
-        task6_0 = (byte) (task6_1 + task6_2);
-        System.out.println(task6_0);
-        //task 7
-        byte task7_0, task7_1 = 7;
-        short task7_2 = 10, task7_3 = 5;
-        task7_0 = (byte) ((task7_2 + task7_3) / task7_1);
-        System.out.println(task7_0);
-        //task 8
-        int task8 = (task7_2 * task7_3) / (task7_0 * task7_1);
-        System.out.println(task8);
-        //task9
-        byte a = 2, b = 3, c;
-        c = a;
-        a = b;
-        b = c;
-        System.out.println("task9: " + a + " " + b);
-        //task10
-        byte a1 = 2, b1 = 3;
-        a1 = (byte) (a1 + b1);
-        b1 = (byte) (a1 - b1);
-        a1 = (byte) (a1 - b1);
-        System.out.println("task10: " + a1 + " " + b1);
-
 
     }
 }
