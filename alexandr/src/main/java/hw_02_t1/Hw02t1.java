58b90cfdf1bb2097095167c81600d7f2ce1b3a28
SKnice <kaverins@ukr.net>
2017-01-15 15:57:27 +0200

    #7  Home tasks. Пример сканера.

---
+++ alexandr/src/main/java/hw_02_t1/Hw02t1.java
@@ -0,0 +1,17 @@
+package hw_02_t1;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class Hw02t1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int i = scan.nextInt();
+
+    }
+
+
+}
