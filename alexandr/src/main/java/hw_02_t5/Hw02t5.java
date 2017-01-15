ce36ea071f28b8e4220f8d6df216b3cf82336013
SKnice <kaverins@ukr.net>
2017-01-15 22:54:12 +0200

    #12  Квадратное уравнение.

---
+++ alexandr/src/main/java/hw_02_t5/Hw02t5.java
@@ -0,0 +1,12 @@
+package hw_02_t5;
+
+/**
+ * Задано четыре переменных, найти и вывести на экран вторую по величине (значения всех переменных разные).
+ */
+public class Hw02t5 {
+    public static void main(String[] args) {
+        int digit1, digit2, digit3, digit4;
+
+    }
+
+}
