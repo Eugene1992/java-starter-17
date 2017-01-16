9c10610a936f6c654525bef9c6368369131449e4
Igor Malyshko <malishkoio@gmail.com>
2017-01-16 21:28:47 +0200

    #17 HW_03_t2

---
+++ igor/src/main/java/hw_03/Hw_03_t2.java
@@ -0,0 +1,11 @@
+package hw_03;
+
+/**
+ * Created by ihor on 16.01.2017.
+ * Выведите на экран первые 20 членов последовательности Фибоначчи.
+ */
+public class Hw_03_t2 {
+    public static void main(String[] args) {
+        //todo: узнать про фибоначи
+    }
+}
