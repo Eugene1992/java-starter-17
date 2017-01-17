c1695be536c85fcc5e94581305248b72adf7098e
SKnice <kaverins@ukr.net>
2017-01-17 22:37:48 +0200

    #17 Fibonachy's digitals

---
+++ alexandr/src/main/java/hw_03/Hw03t2.java
@@ -0,0 +1,15 @@
+package hw_03;
+
+/**
+ * Выведите на экран первые 20 членов последовательности Фибоначчи.
+ */
+public class Hw03t2 {
+    public static void main(String[] args) {
+        for (int temp1 = 1, temp2 = 0, temp3 = 0, i = 1; i < 21; i++) {
+            temp3 = temp1 + temp2;
+            System.out.println(i + ". " + temp3);
+            temp1 = temp2;
+            temp2 = temp3;
+        }
+    }
+}
