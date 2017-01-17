0d2a260cbd5081bc71066b34b2222e55d9b74fcf
SKnice <kaverins@ukr.net>
2017-01-18 00:39:55 +0200

    #19 Факториал введенного числа пользователем.

--- alexandr/src/main/java/hw_03/Hw03t4_Factorrrrial.java
+++ alexandr/src/main/java/hw_03/Hw03t4_Factorrrrial.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Hw03t4_Factorrrrial {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Введите число для вічисления факториала: ");
+        System.out.print("Введите число для вычисления факториала: ");
         int fac = scan.nextInt();
         int temp = 1;
         for (int i = 1; i < fac + 1; i++) {
