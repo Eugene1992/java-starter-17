c4b6642374bf20eeddbf8abeb55a748f74ed4ee6
Igor Malyshko <malishkoio@gmail.com>
2017-01-18 18:44:40 +0200

    CW_05 ihor

---
+++ igor/src/main/java/metod/Metod.java
@@ -0,0 +1,33 @@
+package metod;
+
+import java.util.Scanner;
+
+/**
+ * Created by ihor on 18.01.2017.
+ * test Metod
+ */
+public class Metod {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //int quantity = scanner.nextInt();
+        //String str = scanner.next();
+        //printStr(quantity, str);
+        int start = scanner.nextInt(), stop = scanner.nextInt(), step = scanner.nextInt();
+        int sum = calcSumInterval(start, stop, step);
+        System.out.println(sum);
+    }
+
+    static void printStr(int count, String str) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(i + 1 + " " + str);
+        }
+    }
+
+    static int calcSumInterval(int start, int stop, int step) {
+        int sum = 0;
+        for (int i = start; i <= stop; i += step) {
+            sum += i;
+        }
+        return sum;
+    }
+}
---
+++ igor/src/main/java/metod/Metod_02.java
@@ -0,0 +1,38 @@
+package metod;
+
+import java.util.Scanner;
+
+/**
+ * Created by ihor on 18.01.2017.
+ * metod 02
+ */
+public class Metod_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        random(scanner.nextInt());
+    }
+    static void random (int count){
+        for (int i = 0; i < count; i++) {
+            godOrBed((int)( Math.random()*1000));
+        }
+    }
+    static void godOrBed (int ran){
+        switch (ran){
+            case 7: {}
+            case 777: {}
+            case 3: {printNumber(true, ran);  break;}
+            case 13: {}
+            case 33: {}
+            case 666: {printNumber(false, ran); break;}
+        }
+    }
+
+    static void printNumber  (boolean str, int number) {
+        if (str) {
+            System.out.println(number + " god");
+        } else {
+            System.out.println(number + " bed");
+        }
+    }
+}
+
