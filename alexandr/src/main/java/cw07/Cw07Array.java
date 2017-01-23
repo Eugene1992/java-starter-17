e3660acf01cfcb1eba137ed580fe4e4f11048f5d
Kaverin <oleksandr_kaverin@epam.com>
2017-01-24 01:13:33 +0200

    Max and Min digits in array

--- alexandr/src/main/java/cw07/Cw07Array.java
+++ alexandr/src/main/java/cw07/Cw07Array.java
@@ -14,8 +14,8 @@ public class Cw07Array {
                 scan.nextInt(),
                 scan.nextInt()};
         //System.out.println(arr(array));
-        // arr(array);
-        System.out.println("Max digit = " + arrMax(array)[0] + " Min digital = " +arrMax(array)[1]);
+        // arr(array);          // 1. Написать метод который принимает масив и выводит его сумму
+        System.out.println("Max digit = " + arrMax(array)[0] + " Min digital = " + arrMax(array)[1]);
     }
 
     static void arr(int[] temp) {
@@ -27,34 +27,31 @@ public class Cw07Array {
         }
         System.out.print("]");
     }
-// todo : Найти максимальное и минимальное число в массиве. !!!!!
-    static int [] arrMax(int[] temp) {
-        int max = 0, min = 0;
 
-        for (int i = 0; i < temp.length; i++) {
-            for (int j = 0; j < temp.length; j++) {
-                if (temp[i] > temp[j]) {
-                    max = temp[i];
-                }
-                else {max = temp[j];}
-                if (temp[i] < temp[j]) {
-                    min = temp[i];
-                }
-                else {min = temp[j];}
+    // todo : Найти максимальное и минимальное число в массиве. !!!!!
+    static int[] arrMax(int[] temp) {
+        int max = temp[0], min = temp[1];
 
-                }
+        for (int j = 0; j < temp.length; j++) {
+            if (min > temp[j]) {
+                min = temp[j];
             }
-        int [] maxMin = new int [2];
+            if (max < temp[j]) {
+                max = temp[j];
+            }
+        }
+        int[] maxMin = new int[2];
         maxMin[0] = max;
         maxMin[1] = min;
         return maxMin;
     }
 
-    static void digits13 () {
-        int count = 0;
-        for (int i = 0; i < 999999 ; i++) {
-            if (i % 100000 > 0 ) { count ++;
 
+    static void digits13() {
+        int count = 0;
+        for (int i = 0; i < 999999; i++) {
+            if (i % 100000 > 0) {
+                count++;
 
 
             }
