37054e2dbfef729884df51557db24304d90d80a2
Igor Malyshko <malishkoio@gmail.com>
2017-01-26 10:32:21 +0200

    cw array

---
+++ igor/src/main/java/arrays/CW_arr.java
@@ -0,0 +1,46 @@
+package arrays;
+
+/**
+ * Created by ihor on 23.01.2017.
+ * CW
+ */
+public class CW_arr {
+    public static void main(String[] args) {
+        /*int[] arr = new int[10];
+        arr = randArr(arr, 100);
+        System.out.println(printArr(arr));
+        System.out.println("\n" + sumArr(arr));*/
+        
+    }
+
+
+     public static int[] randArr(int length, int b) {
+        int[] array = new int[length];
+        int a = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (byte) (a + (Math.random() * b));
+        }
+        return array;
+    }
+
+    public static String printArr(int[] array) {
+        String str = "[";
+        for (int i = 0; i < array.length; i++) {
+            str += array[i];
+            if (i < array.length - 1){
+                str += ", ";
+            }   else {
+                str += "]";
+            }
+        }
+        return str;
+    }
+
+    static int sumArr(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+}
---
+++ igor/src/main/java/method/Recursion.java
@@ -0,0 +1,64 @@
+package method;
+
+import java.util.Scanner;
+
+/**
+ * Created by ihor on 20.01.2017.
+ */
+
+
+public class Recursion {
+    static int counter = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int startNumber = 0;
+        //f(startNumber);
+        //int result = fakt(5);
+        System.out.println(passwordGenerator(scanner.nextInt()));
+    }
+
+    static void f(int i) {
+        counter++;
+        if (i <= 10 & counter <= 10) {
+            i++;
+        } else {
+            i--;
+        }
+        System.out.print(i + " ");
+        if (counter < 19) {
+            f(i);
+        } else {
+            return;
+        }
+        return;
+    }
+
+    static int fakt(int number) {
+        int result = 1;
+        if (number < 1) {
+            return result;
+        }
+        result = number * fakt((number - 1));
+        return result;
+    }
+
+    static String passwordGenerator (int coutner) {
+        String pass = "";
+        for (int i = 0; i < coutner; i++) {
+            pass+= randChar();
+        }
+        return pass;
+    }
+
+    static char randChar (){
+        int a = 48, b = 122;
+        char ch = 0;
+        int number =(int) (a + (Math.random() * b));
+        while ((48 <= number & number <= 57) | (65 <= number & number <= 90) | (97 <= number & number <= 122)) {
+            ch = (char) (a + Math.random() * b);
+        }
+        return ch;
+    }
+}
+
