0b41a16314e3cf4080923b0ca2a9d9feba6bb7aa
SKnice <kaverins@ukr.net>
2017-01-27 17:15:04 +0200

    5 сортудников, у кого больше ЗП

---
+++ alexandr/src/main/java/cw09/Cw09.java
@@ -0,0 +1,23 @@
+package cw09;
+
+/**
+ * Создать сотрудников с : ИМЯ, ЗП, Возрост
+ */
+public class Cw09 {
+    public static void main(String[] args) {
+        String name1 = "Sasha"; int zp1 = 1000; int age1 = 20;
+        String name2 = "Petya"; int zp2 = 1000; int age2 = 20;
+        String name3 = "Kolya"; int zp3 = 2000; int age3 = 20;
+        String name4 = "Katya"; int zp4 = 5000; int age4 = 20;
+        String name5 = "Yulya"; int zp5 = 1000; int age5 = 20;
+
+
+        for (int i = 0; i <5; i++) {
+
+        }
+    }
+
+    static void employee(String name, int zp, int age) {
+
+    }
+}
